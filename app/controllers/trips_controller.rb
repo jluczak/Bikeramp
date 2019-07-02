acee74dc8c3ffdd4103b129af82d88601e8b999b
Joanna <joanna@Joannas-MacBook-Pro.local>
2019-07-02 13:51:47 +0200

    Add actions to Trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,16 +1,43 @@
 class TripsController < ApplicationController
   def show
+    render json: trip.to_json(only: serialized_params)
   end
 
   def index
+    trips = Trip.all
+
+    render json: trips.to_json(only: serialized_params)
   end
 
   def create
+    trip = Trip.create!(trip_params)
+
+    render json: trip.to_json(only: serialized_params), status: 201
   end
 
   def update
+    trip.update!(trip_params)
+
+    render json: trip.to_json(only: serialized_params)
   end
 
   def destroy
+    trip.destroy!
+
+    head 204
+  end
+
+  private
+
+  def trip
+    trip ||= Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.permit(:start_address, :destination_address, :price)
+  end
+
+  def serialized_params
+    [:start_address, :destination_address, :price]
   end
 end
