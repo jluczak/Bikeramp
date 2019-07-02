aa1f63ec01f896fb6f639935dc71d70420d93ac4
Joanna <joanna@Joannas-MacBook-Pro.local>
2019-07-02 16:08:02 +0200

    Add TripSerializer

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,24 +1,24 @@
 class TripsController < ApplicationController
   def show
-    render json: trip.to_json(only: serialized_params)
+    render json: TripSerializer.new(trip)
   end
 
   def index
     trips = Trip.all
 
-    render json: trips.to_json(only: serialized_params)
+    render json: TripSerializer.new(trips)
   end
 
   def create
     trip = Trip.create!(trip_params)
 
-    render json: trip.to_json(only: serialized_params), status: 201
+    render json: TripSerializer.new(trip), status: 201
   end
 
   def update
     trip.update!(trip_params)
 
-    render json: trip.to_json(only: serialized_params)
+    render json: TripSerializer.new(trip)
   end
 
   def destroy
@@ -33,9 +33,5 @@ class TripsController < ApplicationController
 
   def trip_params
     params.permit(:start_address, :destination_address, :price)
-  end
-
-  def serialized_params
-    %i[start_address destination_address price distance]
   end
 end
