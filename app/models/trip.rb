cd79d05a40c068d39d1a1ed4f0a4733e12b4fda8
Joanna <joanna@Joannas-MacBook-Pro.local>
2019-07-02 12:01:47 +0200

    Add model tests for Trip, delete presence validation for price

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,4 @@
 class Trip < ApplicationRecord
-  validates_presence_of :start_address, :destination_address, :price
+  validates_presence_of :start_address, :destination_address
   validates :price, numericality: { greater_than: 0 }
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'column presence and type' do
+    it { is_expected.to have_db_column(:id) }
+    it { is_expected.to have_db_column(:start_address).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:destination_address).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:price).of_type(:decimal).with_options(precision: 6, scale: 2, null: false) }
+  end
+
+  context 'validation' do
+    it { is_expected.to validate_presence_of(:start_address) }
+    it { is_expected.to validate_presence_of(:destination_address) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
+  end
 end
