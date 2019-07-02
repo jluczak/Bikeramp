b80d0bbcd9579a2f82fd522eff151b7e20b63c78
Joanna <joanna@Joannas-MacBook-Pro.local>
2019-07-02 11:54:52 +0200

    Add validations and constraints to Trip

--- db/migrate/20190702095141_create_trips.rb
+++ db/migrate/20190702095141_create_trips.rb
@@ -1,9 +1,9 @@
 class CreateTrips < ActiveRecord::Migration[5.2]
   def change
     create_table :trips do |t|
-      t.string :start_address
-      t.string :destination_address
-      t.decimal :price, precision: 6, scale: 2
+      t.string :start_address, null: false
+      t.string :destination_address, null: false
+      t.decimal :price, precision: 6, scale: 2, null: false
 
       t.timestamps
     end
