fa086868fd11ecf58d135ce50bd1c0f9b38cc38e
Joanna <joanna@Joannas-MacBook-Pro.local>
2019-07-02 14:42:18 +0200

    Add distance parameter to Trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_095141) do
+ActiveRecord::Schema.define(version: 2019_07_02_115429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_095141) do
     t.decimal "price", precision: 6, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "distance", null: false
   end
 
 end
