3f26cfcb33e4a16d40f26e98d3f98934381e0267
Joanna <joanna@Joannas-MacBook-Pro.local>
2019-07-03 17:01:33 +0200

    Configure vcr_setup

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -3,4 +3,5 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.hook_into :webmock
+  c.filter_sensitive_data('<google_api_key>') { ENV['GOOGLE_API_KEY'] }
 end
