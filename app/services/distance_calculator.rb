a2b9b5b637f38f87b5ed160762eb5943389ef3b0
jluczak <jluczaksas@gmail.com>
2019-07-08 17:46:16 +0200

    [refactoring] refactor Distance Calculator

--- app/services/distance_calculator.rb
+++ app/services/distance_calculator.rb
@@ -3,46 +3,25 @@ class DistanceCalculator
     @geocoder = geocoder
     @start_address = start_address
     @destination_address = destination_address
-    @errors = ActiveModel::Errors.new(self)
   end
 
   def call
-    start_point = @geocoder.geocode(@start_address)
-    end_point = @geocoder.geocode(@destination_address)
-    if !start_point.success || !end_point.success
-      raise AddressNotFound.new([
+    start_point = geocoder.geocode(start_address)
+    end_point = geocoder.geocode(destination_address)
+
+    if start_point.success && end_point.success
+      start_point.distance_from(end_point, units: :meters)
+    else
+      messages = [
         start_address: ['Could not find address'],
         destination_address: ['Could not find address']
-      ])
-    else
-      start_point.distance_from(end_point, units: :meters)
+      ]
+
+      raise AddressNotFound, messages
     end
-    # @distance = start_point.distance_from(end_point, units: :meters)
-    # returned_object
   end
 
   private
 
-  attr_accessor :errors, :distance
-
-  # def start_point
-  #   @start_point ||= get_coordinates(@start_address, 'start_address')
-  # end
-  #
-  # def end_point
-  #   @end_point ||= get_coordinates(@destination_address, 'destination_address')
-  # end
-  #
-  # def get_coordinates(location, key)
-  #   @geocoder.geocode(location).tap do |geo|
-  #     errors.add(key, 'Could not find address') unless geo.success
-  #   end
-  # end
-  #
-  # def returned_object
-  #   {
-  #     distance: distance,
-  #     errors: errors
-  #   }
-  # end
+  attr_reader :geocoder, :start_address, :destination_address
 end
