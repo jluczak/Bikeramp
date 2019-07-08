1fcab186e44967bb68512cb1a4ddf209c7add06c
jozzi <dominik.malantowicz@gmail.com>
2019-07-08 10:58:18 +0200

    [refactoring] Extract duplicated code to subject

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'dry-transaction'
 gem 'fast_jsonapi'
 gem 'geokit-rails'
 gem 'pg', '>= 0.18', '< 2.0'
--- spec/requests/trip_create_spec.rb
+++ spec/requests/trip_create_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Trip creation', type: :request do
+  subject { post '/trips', params: params, headers: headers }
+
   let(:headers) { { 'ACCEPT' => 'application/json' } }
+  let(:params) { {} }
+
   let(:json_response) { JSON.parse(response.body) }
 
   it 'has a valid factory' do
@@ -9,20 +13,20 @@ RSpec.describe 'Trip creation', type: :request do
   end
 
   context 'with valid data', vcr: { cassette_name: 'requests/trip_create' } do
-    let(:trip_creation) { post '/trips', params: FactoryBot.attributes_for(:trip), headers: headers }
+    let(:params) { FactoryBot.attributes_for(:trip) }
 
     it 'returns 201 with json format' do
-      trip_creation
+      subject
       expect(response).to have_http_status(201)
       expect(response.content_type).to eq('application/json')
     end
 
     it 'creates a trip' do
-      expect { trip_creation }.to change { Trip.count }.by(1)
+      expect { subject }.to change { Trip.count }.by(1)
     end
 
     it 'returns a trip' do
-      trip_creation
+      subject
       expect(json_response['data']['attributes']).to include(
         'start_address' => 'Plac Europejski 2, Warszawa, Polska',
         'destination_address' => 'Leszno 15, Warszawa, Polska',
@@ -33,24 +37,26 @@ RSpec.describe 'Trip creation', type: :request do
   end
 
   context 'with invalid data', vcr: { cassette_name: 'requests/trip_create' } do
-    let(:trip_creation) do
-      post '/trips', params: { start_address: 'fjsjakdk',
+    let(:params) do
+      {
+        start_address: 'fjsjakdk',
         destination_address: 'kadkdfmfd',
-        price: 3.54 }, headers: headers
+        price: 3.54
+      }
     end
 
     it 'returns 422 with json format' do
-      trip_creation
+      subject
       expect(response).to have_http_status(422)
       expect(response.content_type).to eq('application/json')
     end
 
     it 'does not create a trip' do
-      expect { trip_creation }.to_not change { Trip.count }
+      expect { subject }.to_not change { Trip.count }
     end
 
     it 'returns error' do
-      trip_creation
+      subject
       expect(json_response).to include(
         'destination_address' => ['Could not find address'],
         'start_address' => ['Could not find address']
