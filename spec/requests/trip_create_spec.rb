7df692319455b70740d0cf4d1f05d80fe41d798d
jozzi <dominik.malantowicz@gmail.com>
2019-07-08 10:50:38 +0200

    [refactoring] Use nicer vcr syntax

--- spec/requests/trip_create_spec.rb
+++ spec/requests/trip_create_spec.rb
@@ -8,65 +8,53 @@ RSpec.describe 'Trip creation', type: :request do
     expect(FactoryBot.create(:trip)).to be_valid
   end
 
-  context 'with valid data' do
+  context 'with valid data', vcr: { cassette_name: 'requests/trip_create' } do
     let(:trip_creation) { post '/trips', params: FactoryBot.attributes_for(:trip), headers: headers }
 
     it 'returns 201 with json format' do
-      VCR.use_cassette('requests/trip_create') do
-        trip_creation
-        expect(response).to have_http_status(201)
-        expect(response.content_type).to eq('application/json')
-      end
+      trip_creation
+      expect(response).to have_http_status(201)
+      expect(response.content_type).to eq('application/json')
     end
 
     it 'creates a trip' do
-      VCR.use_cassette('requests/trip_create') do
-        expect { trip_creation }.to change { Trip.count }.by(1)
-      end
+      expect { trip_creation }.to change { Trip.count }.by(1)
     end
 
     it 'returns a trip' do
-      VCR.use_cassette('requests/trip_create') do
-        trip_creation
-        expect(json_response['data']['attributes']).to include(
-          'start_address' => 'Plac Europejski 2, Warszawa, Polska',
-          'destination_address' => 'Leszno 15, Warszawa, Polska',
-          'price' => '4.65',
-          'distance' => '798.76'
-        )
-      end
+      trip_creation
+      expect(json_response['data']['attributes']).to include(
+        'start_address' => 'Plac Europejski 2, Warszawa, Polska',
+        'destination_address' => 'Leszno 15, Warszawa, Polska',
+        'price' => '4.65',
+        'distance' => '798.76'
+      )
     end
   end
 
-  context 'with invalid data' do
+  context 'with invalid data', vcr: { cassette_name: 'requests/trip_create' } do
     let(:trip_creation) do
       post '/trips', params: { start_address: 'fjsjakdk',
-                               destination_address: 'kadkdfmfd',
-                               price: 3.54 }, headers: headers
+        destination_address: 'kadkdfmfd',
+        price: 3.54 }, headers: headers
     end
 
     it 'returns 422 with json format' do
-      VCR.use_cassette('requests/trip_create') do
-        trip_creation
-        expect(response).to have_http_status(422)
-        expect(response.content_type).to eq('application/json')
-      end
+      trip_creation
+      expect(response).to have_http_status(422)
+      expect(response.content_type).to eq('application/json')
     end
 
     it 'does not create a trip' do
-      VCR.use_cassette('requests/trip_create') do
-        expect { trip_creation }.to_not change { Trip.count }
-      end
+      expect { trip_creation }.to_not change { Trip.count }
     end
 
     it 'returns error' do
-      VCR.use_cassette('requests/trip_create') do
-        trip_creation
-        expect(json_response).to include(
-          'destination_address' => ['Could not find address'],
-          'start_address' => ['Could not find address']
-        )
-      end
+      trip_creation
+      expect(json_response).to include(
+        'destination_address' => ['Could not find address'],
+        'start_address' => ['Could not find address']
+      )
     end
   end
 end
--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -1,7 +1,8 @@
 require 'vcr'
 
-VCR.configure do |c|
-  c.cassette_library_dir = 'spec/vcr_cassettes'
-  c.hook_into :webmock
-  c.filter_sensitive_data('<google_api_key>') { ENV['GOOGLE_API_KEY'] }
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('<google_api_key>') { ENV['GOOGLE_API_KEY'] }
+  config.configure_rspec_metadata!
 end
