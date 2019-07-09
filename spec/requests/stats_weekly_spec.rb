c9111407e757bd21ae990d5476961e9e6112f5fc
jluczak <jluczaksas@gmail.com>
2019-07-09 10:51:40 +0200

    add test for weekly total distance

--- spec/requests/stats_weekly_spec.rb
+++ spec/requests/stats_weekly_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
 describe 'Weekly stats', type: :request do
+  subject { get '/stats/weekly' }
+
   let!(:trips) do
     FactoryBot.create_list(:trip, 2)
   end
 
-  subject { get '/stats/weekly' }
+  let(:json_response) { JSON.parse(response.body) }
 
   it 'returns 200 status code' do
     subject
     expect(response).to have_http_status(200)
+  end
+
+  it 'returns weekly total distance' do
+    subject
+    expect(json_response).to include("total_distance": "1597,52km")
   end
 end
