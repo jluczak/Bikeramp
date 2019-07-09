128968fec7466c7fde655c7feaee8e5dd42accbd
jluczak <jluczaksas@gmail.com>
2019-07-09 16:27:37 +0200

    add test for returning total_distance grouped by day

--- spec/requests/stats_monthly_spec.rb
+++ spec/requests/stats_monthly_spec.rb
@@ -7,8 +7,9 @@ describe 'monthly stats', type: :request do
   let(:current_month) { DateTime.current.strftime('%m') }
 
   let!(:trip) { FactoryBot.create(:trip, created_at: "2019-#{current_month}-02") }
-  let!(:trip2) { FactoryBot.create(:trip, created_at: "2019-#{current_month}-27") }
-  let!(:trip3) { FactoryBot.create(:trip, created_at: 1.month.ago) }
+  let!(:trip2) { FactoryBot.create(:trip, created_at: "2019-#{current_month}-02", distance: "100.0") }
+  let!(:trip3) { FactoryBot.create(:trip, created_at: "2019-#{current_month}-27") }
+  let!(:trip4) { FactoryBot.create(:trip, created_at: 1.month.ago) }
 
   it 'returns 200 status code' do
     subject
@@ -24,6 +25,12 @@ describe 'monthly stats', type: :request do
   it 'returns only days from current month' do
     subject
     expect(json_response.count).to eq(2)
+  end
+
+  it 'returns total_distance grouped by day' do
+    subject
+    expect(json_response[0]).to include({ "total_distance" => "898.76" })
+    expect(json_response[1]).to include({ "total_distance" => "798.76" })
   end
 
 end
