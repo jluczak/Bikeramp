3b120890ec917b1f8acbd4b16e7110539a935bbc
jluczak <jluczaksas@gmail.com>
2019-07-09 10:25:00 +0200

    add tests for weekly statistics status code

---
+++ spec/requests/statistics_weekly_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'Weekly statistics', type: :request do
+  let!(:trips) do
+    FactoryBot.create_list(:trip, 2)
+  end
+
+  subject { get "/stats/weekly" }
+
+  it 'returns 200 status code' do
+    expect(subject).to have_http_status(200)
+  end
+end
