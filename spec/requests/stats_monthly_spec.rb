5d09c04df717b3ac096cc8179278080a3fe716ea
jluczak <jluczaksas@gmail.com>
2019-07-09 16:11:19 +0200

    fix tests for returning only days where trips where made

--- spec/requests/stats_monthly_spec.rb
+++ spec/requests/stats_monthly_spec.rb
@@ -4,21 +4,21 @@ describe 'monthly stats', type: :request do
   subject { get '/stats/monthly' }
 
   let(:json_response) { JSON.parse(response.body) }
+  let(:current_month) { DateTime.current.strftime('%m') }
+
+  let!(:trip) { FactoryBot.create(:trip, created_at: "2019-#{current_month}-02") }
+  let!(:trip2) { FactoryBot.create(:trip, created_at: "2019-#{current_month}-27") }
 
   it 'returns 200 status code' do
     subject
     expect(response).to have_http_status(200)
   end
 
-  context 'with trips from current month only' do
-    let!(:trips) do
-      FactoryBot.create_list(:trip, 2)
-    end
-    let(:today) { Date.today }
 
-    it 'returns all days from current month where trips where made' do
-      subject
-      expect(json_response).to include("day" => "#{today.strftime("%B, #{today.day.ordinalize}")}")
-    end
+  it 'returns only days where trips where made' do
+    subject
+    expect(json_response[0]).to include({ "day" => "2019-#{current_month}-02" })
+    expect(json_response[1]).to include({ "day" => "2019-#{current_month}-27" })
   end
+
 end
