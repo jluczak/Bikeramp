d46a584176a925b95e5ae111c3850678cf41c4cf
jluczak <jluczaksas@gmail.com>
2019-07-09 17:12:42 +0200

    [refactoring] delete redundant scopes

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,4 @@
 class Trip < ApplicationRecord
   validates_presence_of :start_address, :destination_address
   validates :price, :distance, numericality: { greater_than: 0 }
-
-  scope :weekly_distance, -> { WeeklyStatsQuery.weekly_distance }
-  scope :weekly_price, -> { WeeklyStatsQuery.weekly_price }
 end
