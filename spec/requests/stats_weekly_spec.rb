f60ff84cfbf6368db376e837e1deb8f621c50bf6
jluczak <jluczaksas@gmail.com>
2019-07-09 11:38:15 +0200

    add test for returning distance only from this week's trips

--- spec/requests/stats_weekly_spec.rb
+++ spec/requests/stats_weekly_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 describe 'Weekly stats', type: :request do
   subject { get '/stats/weekly' }
 
-  let!(:trips) do
-    FactoryBot.create_list(:trip, 2)
-  end
-
   let(:json_response) { JSON.parse(response.body) }
 
   it 'returns 200 status code' do
@@ -14,8 +10,25 @@ describe 'Weekly stats', type: :request do
     expect(response).to have_http_status(200)
   end
 
-  it 'returns weekly total distance' do
-    subject
-    expect(json_response).to include("total_distance": "1597,52km")
+  context 'with trips from current week only' do
+    let!(:trips) do
+      FactoryBot.create_list(:trip, 2)
+    end
+
+    it 'returns weekly total distance' do
+      subject
+      expect(json_response).to include("total_distance" => "1597.52km")
+    end
+  end
+
+  context 'with trips older that week' do
+    let!(:trip) { FactoryBot.create(:trip, created_at: 2.weeks.ago) }
+    let!(:trip) { FactoryBot.create(:trip, created_at: 1.month.ago) }
+    let!(:trip) { FactoryBot.create(:trip, created_at: 1.year.ago) }
+
+    it 'returns weekly total distance' do
+      subject
+      expect(json_response).to include("total_distance" => "0.0km")
+    end
   end
 end
