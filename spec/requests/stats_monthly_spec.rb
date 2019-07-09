9fca6484915c1453875996a656e1aae9db69e18a
jluczak <jluczaksas@gmail.com>
2019-07-09 16:19:12 +0200

    add test for returning only days from current month

--- spec/requests/stats_monthly_spec.rb
+++ spec/requests/stats_monthly_spec.rb
@@ -8,17 +8,22 @@ describe 'monthly stats', type: :request do
 
   let!(:trip) { FactoryBot.create(:trip, created_at: "2019-#{current_month}-02") }
   let!(:trip2) { FactoryBot.create(:trip, created_at: "2019-#{current_month}-27") }
+  let!(:trip3) { FactoryBot.create(:trip, created_at: 1.month.ago) }
 
   it 'returns 200 status code' do
     subject
     expect(response).to have_http_status(200)
   end
 
-
   it 'returns only days where trips where made' do
     subject
     expect(json_response[0]).to include({ "day" => "2019-#{current_month}-02" })
     expect(json_response[1]).to include({ "day" => "2019-#{current_month}-27" })
+  end
+
+  it 'returns only days from current month' do
+    subject
+    expect(json_response.count).to eq(2)
   end
 
 end
