12eb67490f1283a07d7f06179ad1b40fec8cb04f
jluczak <jluczaksas@gmail.com>
2019-07-09 11:38:43 +0200

    return total weekly distance

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,6 +1,7 @@
 class StatsController < ApplicationController
   def weekly
-    render json: "stats", status: 200
+    total_distance = Trip.where(created_at: 1.week.ago..Time.current).sum(:distance)
+    render json: {total_distance: "#{total_distance}km"}, status: 200
   end
 
   def monthly
