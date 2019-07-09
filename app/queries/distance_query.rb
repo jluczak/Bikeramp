ff4e374bb944cc3466b82e20f961ba638e71a820
jluczak <jluczaksas@gmail.com>
2019-07-09 16:35:14 +0200

    return monthly stats with price summed

--- app/queries/distance_query.rb
+++ app/queries/distance_query.rb
@@ -9,10 +9,12 @@ class DistanceQuery
     today = DateTime.current
     current_month_first_day = today.beginning_of_month
     relation
-      .select('date(created_at) as day, sum(distance) as total_distance')
+      .select('date(created_at) as day,
+               sum(distance) as total_distance,
+               sum(price) as total_price')
       .where('created_at > ?', current_month_first_day)
       .group('date(created_at)')
       .order('date(created_at)')
-      .as_json(:except => :id)
+      .as_json(except: :id)
   end
 end
