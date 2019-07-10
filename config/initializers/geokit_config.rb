5609c4eebdbbaa42cdef8d2a1c0c54f6c65b794c
jluczak <jluczaksas@gmail.com>
2019-07-10 16:58:20 +0200

    [refactoring] fetch ENVs

--- config/initializers/geokit_config.rb
+++ config/initializers/geokit_config.rb
@@ -2,5 +2,5 @@ Geokit.default_units = :meters # others :kms, :nms, :meters
 Geokit.default_formula = :sphere
 Geokit::Geocoders.request_timeout = 3
 
-Geokit::Geocoders::GoogleGeocoder.api_key = ENV['GOOGLE_API_KEY']
+Geokit::Geocoders::GoogleGeocoder.api_key = ENV.fetch('GOOGLE_API_KEY')
 Geokit::Geocoders.provider_order = [:google]
